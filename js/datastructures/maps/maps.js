99141c6ce4e6774f37154b03cd3892807151aebd
Shad <shad.amezng@gmail.com>
2025-01-25 22:44:36 +0530

    added the maps practice files

---
+++ js/datastructures/maps/maps.js
@@ -0,0 +1,19 @@
+/**
+ * Assignment 1: Create a Map and Access Values
+ */
+
+/**
+ * Assignment 2: Check if a Key Exists
+ */
+
+/**
+ * Assignment 3: Map Size
+ */
+
+/**
+ * Assignment 4: Iterating Through a Map
+ */
+
+/**
+ * Assignment 5: Remove Items from a Map
+ */
